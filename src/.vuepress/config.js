20a6a8ec5e10792955a0a7afec373f99f14a0907
Yegor Gunko <57867800+yegorgunko@users.noreply.github.com>
2021-03-11 19:44:45 +0200

    Добавлена иконка

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -22,6 +22,7 @@ module.exports = {
       'meta',
       { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
     ],
+    ['link', { rel: 'icon', href: '/favicon.png' }],
   ],
 
   /**
