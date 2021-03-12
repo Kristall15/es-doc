3c9001d981e59f753b1251a7babeb347fd9fdfe7
Yegor Gunko <57867800+yegorgunko@users.noreply.github.com>
2021-03-12 16:46:40 +0200

    Изменена конфигурация плагина для просмотра изображений
    Добавлен тёмный фон и курсор при наведении

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -50,5 +50,16 @@ module.exports = {
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
-  plugins: ['@vuepress/plugin-back-to-top', '@vuepress/plugin-medium-zoom'],
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    [
+      '@vuepress/plugin-medium-zoom',
+      {
+        options: {
+          margin: 24,
+          background: '#212121',
+        },
+      },
+    ],
+  ],
 }
