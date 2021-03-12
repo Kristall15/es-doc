530236da8ffe4de3ea5467fed52ccef10f95ca3a
Yegor Gunko <57867800+yegorgunko@users.noreply.github.com>
2021-03-12 11:28:12 +0200

    Добавлены списки звуков из игры

---
+++ src/.vuepress/components/AmbiencesTable.vue
@@ -0,0 +1,64 @@
+<template>
+  <table>
+    <tr>
+      <th>Код</th>
+      <th>Предпросмотр</th>
+    </tr>
+    <tr v-for="(name, index) in ambiences" :key="index">
+      <td>
+        <code>play ambience {{ name }}</code>
+      </td>
+      <td>
+        <audio :src="`/ambiences/${name}.ogg`" controls></audio>
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ambiences: [
+        'boat_station_day',
+        'boat_station_night',
+        'camp_center_day',
+        'camp_center_evening',
+        'camp_center_night',
+        'camp_entrance_day',
+        'camp_entrance_day_people',
+        'camp_entrance_night',
+        'catacombs',
+        'catacombs_stones',
+        'clubs_inside_day',
+        'cold_wind_loop',
+        'day_countryside_ambience',
+        'dining_hall_empty',
+        'dining_hall_full',
+        'ext_road_day',
+        'ext_road_evening',
+        'ext_road_night',
+        'forest_day',
+        'forest_evening',
+        'forest_night',
+        'grasshoper_clean',
+        'int_cabin_day',
+        'int_cabin_evening',
+        'int_cabin_night',
+        'lake_shore_day',
+        'lake_shore_evening',
+        'lake_shore_night',
+        'library_day',
+        'medium_crowd_indoors_1',
+        'medium_crowd_outdoors',
+        'medstation_inside_day',
+        'medstation_inside_night',
+        'music_club_day',
+        'music_club_night',
+        'old_camp_outside',
+        'soccer_play_background',
+      ],
+    }
+  },
+}
+</script>
---
+++ src/.vuepress/components/MusicTable.vue
@@ -0,0 +1,99 @@
+<template>
+  <table>
+    <tr>
+      <th>Код</th>
+      <th>Предпросмотр</th>
+    </tr>
+    <tr v-for="(name, index) in music" :key="index">
+      <td>
+        <code>play music music_list["{{ name }}"]</code>
+      </td>
+      <td>
+        <audio :src="`/music/${name}.ogg`" controls></audio>
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      music: [
+        '410',
+        'a_promise_from_distant_days',
+        'a_promise_from_distant_days_v2',
+        'afterword',
+        'always_ready',
+        'awakening_power',
+        'blow_with_the_fires',
+        'confession',
+        'dance_of_fireflies',
+        'dinner_horn_processed',
+        'doomed_to_be_defeated',
+        'door_to_nightmare',
+        'drown',
+        'eat_some_trouble',
+        'eternal_longing',
+        'everlasting_summer',
+        'everlasting_summer_op_edition',
+        'faceless',
+        'farewell_to_the_past',
+        'farewell_to_the_past_short',
+        'feeling_good',
+        'forest_maiden',
+        'gentle_predator',
+        'get_to_know_me_better',
+        'glimmering_coals',
+        'goodbye_home_shores',
+        'heather',
+        'i_dont_blame_you',
+        'i_want_to_play',
+        'into_the_unknown',
+        'just_think',
+        'kostry',
+        'lets_be_friends',
+        'lightness',
+        'lightness_radio',
+        'meet_me_there',
+        'memories',
+        'memories_piano_outdoors',
+        'miku_song_flute',
+        'miku_song_learn1',
+        'miku_song_voice',
+        'my_daily_life',
+        'mystery_girl',
+        'no_tresspassing',
+        'orchid',
+        'pile',
+        'raindrops',
+        'reflection_on_water',
+        'reminiscences',
+        'revenga',
+        'scarytale',
+        'she_is_kind',
+        'silhouette_in_sunset',
+        'smooth_machine',
+        'so_good_to_be_careless',
+        'sparkles',
+        'sunny_day',
+        'sweet_darkness',
+        'take_me_beautifully',
+        'that_s_our_madhouse',
+        'timid_girl',
+        'torture',
+        'trapped_in_dreams',
+        'tried_to_bring_it_back',
+        'two_glasses_of_melancholy',
+        'vzveites_kostrami',
+        'waltz_of_doubts',
+        'went_fishing_caught_a_girl',
+        'what_do_you_think_of_me',
+        'you_lost_me',
+        'you_won_t_let_me_down',
+        'your_bright_side',
+      ],
+    }
+  },
+}
+</script>
